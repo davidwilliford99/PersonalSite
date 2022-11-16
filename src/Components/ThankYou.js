02baa83df99c8de27f0f39d7b65e002426429ef6
davidwilliford99 <d9899w@gmail.com>
2022-11-15 22:31:54 -0500

    Trying to view TY page

--- src/Components/ThankYou.js
+++ src/Components/ThankYou.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ThankYou() {
   return (
-    <div className='h-full 2xl:h-screen bg-zinc-900 flex'>
+    <div className='h-screen bg-zinc-900 flex'>
 
       <h1>Thanks for reaching out!!</h1>
     </div>
